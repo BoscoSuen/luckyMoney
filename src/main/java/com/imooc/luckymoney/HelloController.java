153b593fd32bf2b71a7c5f47071b69cdbcc8a626
Yukinichi <boscosuen@outlook.com>
2019-12-18 23:47:12 -0800

    connected to database

---
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -0,0 +1,23 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Autowired
+    private LimitConfig limitConfig;
+
+    @GetMapping("/say")
+//    @PostMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+//        return "desc: " + limitConfig.getDescription();
+        return "id: " + id;
+    }
+}
---
+++ src/main/java/com/imooc/luckymoney/LuckymoneyController.java
@@ -0,0 +1,61 @@
+package com.imooc.luckymoney;
+
+import com.sun.org.apache.bcel.internal.generic.LUSHR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class LuckymoneyController {
+
+    @Autowired
+    private LuckymoneyRepository repository;
+
+    /**
+     * money list
+     */
+    @GetMapping("/luckymoneys")
+    public List<Luckymoney> list() {
+        return repository.findAll();
+    }
+
+    /**
+     * create luckymoney
+     */
+    @PostMapping("/luckymoneys")
+    public Luckymoney create(@RequestParam("producer") String producer,
+                             @RequestParam("money")BigDecimal money) {
+        Luckymoney luckymoney = new Luckymoney();
+        luckymoney.setProducer(producer);
+        luckymoney.setMoney(money);
+        return repository.save(luckymoney);
+    }
+
+    /**
+     * search by id
+     */
+    @GetMapping("luckymoneys/{id}")
+    public Luckymoney findById(@PathVariable("id") Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    /**
+     * update
+     */
+    @PutMapping("/luckymoneys/{id}")
+    public Luckymoney update(@PathVariable("id") Integer id,
+                             @RequestParam("consumer") String consumer) {
+        Optional<Luckymoney> optional = repository.findById(id);
+        if(optional.isPresent()) {
+            Luckymoney luckymoney =  optional.get();
+            luckymoney.setId(id);
+            luckymoney.setConsumer(consumer);
+            return repository.save(luckymoney);
+        }
+
+        return null;
+    }
+}
